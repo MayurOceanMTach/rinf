9292975fb99129cac8d09b3875a39e523fc1c872
nikunjOceanmtech <nikunjoceanmtech07@gmail.com>
2024-06-06 09:52:11 +0530

    try catch add
    try catch add

--- flutter_ffi_plugin/lib/src/load_os.dart
+++ flutter_ffi_plugin/lib/src/load_os.dart
@@ -17,15 +17,40 @@ DynamicLibrary loadRustLibrary() {
 
   // Otherewise, use the default path.
   if (io.Platform.isLinux) {
-    return DynamicLibrary.open('libhub.so');
+    try {
+      return DynamicLibrary.open('libhub.so');
+    } catch (e) {
+      print("========$e");
+      throw UnsupportedError('This operating system is not supported.');
+    }
   } else if (io.Platform.isAndroid) {
-    return DynamicLibrary.open('libhub.so');
+    try {
+      return DynamicLibrary.open('libhub.so');
+    } catch (e) {
+      print("========$e");
+      throw UnsupportedError('This operating system is not supported.');
+    }
   } else if (io.Platform.isWindows) {
-    return DynamicLibrary.open('hub.dll');
+    try {
+      return DynamicLibrary.open('hub.dll');
+    } catch (e) {
+      print("========$e");
+      throw UnsupportedError('This operating system is not supported.');
+    }
   } else if (io.Platform.isIOS) {
-    return DynamicLibrary.open('rinf.framework/rinf');
+    try {
+      return DynamicLibrary.open('rinf.framework/rinf');
+    } catch (e) {
+      print("========$e");
+      throw UnsupportedError('This operating system is not supported.');
+    }
   } else if (io.Platform.isMacOS) {
-    return DynamicLibrary.open('rinf.framework/rinf');
+    try {
+      return DynamicLibrary.open('rinf.framework/rinf');
+    } catch (e) {
+      print("========$e");
+      throw UnsupportedError('This operating system is not supported.');
+    }
   } else {
     throw UnsupportedError('This operating system is not supported.');
   }
